4581dd9f9bdef47f825528516c300c957bb16dfa
Migz Estrella <migz.zgim@gmail.com>
2018-09-26 11:15:38 +0800

    Added argparse for user input

--- PokerCalcMain.py
+++ PokerCalcMain.py
@@ -1,11 +1,34 @@
+import argparse
 from configparser import ConfigParser
 import logging.config
+import sys
 
 from deuces.Card import Card
 from deuces.Evaluator import Evaluator
 from lib.checkers import input_type_checker
 
 log = logging.getLogger(__name__)
+
+def handle_args(args):
+    """
+    Handles the arguments passed by the user. Used mainly for card input.
+    :return: argParse.ArgumentParser()
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Poker Calculater. Please input your cards:')
+
+    parser.add_argument('-b', '--board', action='store', nargs='*', default=None,
+                        dest='board', help='The board cards.')
+
+    parser.add_argument('-c', '--hand', action='store', nargs=2, default=None,
+                        dest='hand', help='The hand cards.')
+
+    if len(args) < 1:
+        parser.print_help()
+        sys.exit(1)
+
+    return parser.parse_args(args)
 
 
 def calculate_deuces(board_arg, hand_arg):
@@ -42,8 +65,21 @@ if __name__ == '__main__':
                               fname='./logs/logging_config.ini',
                               defaults={'logfilename': config.get('logs', 'path')})
 
-    board = ['4h', '5h', '8h', '7h', '9c']
-    hand = ['2s', '3s']
 
-    print(calculate_deuces(board, hand))
+    # arg handling
+    user_input_cards = handle_args(sys.argv[1:])
+    if user_input_cards.board == None or user_input_cards.hand == None:
+        log.error('Please supply board cards and hand cards.')
+
+    else:
+
+
+
+        print(user_input_cards.board)
+        print(user_input_cards.hand)
+
+        board = ['4h', '5h', '8h', '7h', '9c']
+        hand = ['2s', '3s']
+
+        print(calculate_deuces(board, hand))
 
