e5bdbf56ba74a55c2c1305632fc23a7f03140341
Migz Estrella <migz.zgim@gmail.com>
2018-10-10 10:42:43 +0800

    added mydeck for monte carlo simulation

--- PokerCalcMain.py
+++ PokerCalcMain.py
@@ -6,6 +6,7 @@ import sys
 from deuces.Card import Card
 from deuces.Evaluator import Evaluator
 from lib.checkers import input_type_checker
+from lib.mydeck import MyDeck
 
 log = logging.getLogger(__name__)
 
@@ -57,6 +58,75 @@ def calculate_deuces(board_arg, hand_arg):
         sys.exit(1)
 
 
+def draw_from_deck(number, exclude):
+    """
+    Draws card from a deck
+    :param number:
+    :param exclude:
+    :return:
+    """
+    deck = MyDeck()
+    i = 0
+    drawn_cards = []
+
+    while i != number:
+        sample_draw = deck.draw()
+        if sample_draw in exclude:
+            pass
+        else:
+            drawn_cards.append(sample_draw)
+            i += 1
+
+    return drawn_cards
+
+
+def compute_win_percentage(board_arg, hand_arg):
+    """
+    Computes your winning percentage using a Monte Carlo Simulation
+    :param board_arg: list of string with len > 2
+    :param hand_arg: list of string with len = 2
+    :return:
+    """
+
+    log.debug('Computing win percentage of {} and {}'.format(board_arg, hand_arg))
+    board = []
+    hand = []
+
+    if input_type_checker(board_arg, hand_arg):
+        # convert string to Card object
+        board = list(map(lambda c: Card.new(c), board_arg))
+        hand = list(map(lambda c: Card.new(c), hand_arg))
+
+    simulation_count = 3000
+    win_count = 0
+    runs = 0
+
+    evaluator = Evaluator()
+
+    for sim in range(simulation_count):
+        board_fill = 5 - len(board)
+
+        simulate_board = board + draw_from_deck(board_fill, board + hand)
+        simulate_hand = draw_from_deck(2, simulate_board + hand)
+
+        my_score = evaluator.evaluate(simulate_board, hand)
+        enemy_score = evaluator.evaluate(simulate_board, simulate_hand)
+
+        if my_score < enemy_score:
+            win_count += 1
+
+        runs += 1
+
+        log.debug('Your score: {} {} vs {} {}'.format(
+            evaluator.class_to_string(evaluator.get_rank_class(my_score)),
+            my_score,
+            evaluator.class_to_string(evaluator.get_rank_class(enemy_score)),
+            enemy_score))
+
+    win_chance = win_count/float(runs)
+
+    return win_chance
+
 if __name__ == '__main__':
     # load config and initialize logging
     config = ConfigParser()
@@ -78,4 +148,5 @@ if __name__ == '__main__':
         hand = user_input_cards.hand
 
         print(calculate_deuces(board, hand))
+        print(compute_win_percentage(board, hand))
 
