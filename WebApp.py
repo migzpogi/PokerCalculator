a5e90ea991111e1491a01605e2cda30292a9dfc2
Migz Estrella <migz.zgim@gmail.com>
2018-10-16 22:56:14 +0800

    fixed merge issue

--- WebApp.py
+++ WebApp.py
@@ -71,9 +71,6 @@ def split_hand(hand_args):
     return hand
 
 
-
-
-
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080)
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
+    # app.run()
